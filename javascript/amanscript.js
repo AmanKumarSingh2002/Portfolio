9e902b4a26e7b240eedcb1d1a54cb35e5afbe4db
Aman Kumar Singh <amankumarsingh807661@gmail.com>
2026-01-27 09:47:47 +0530

    initial commit

---
+++ javascript/amanscript.js
@@ -0,0 +1,7 @@
+// typing animation script
+var typed =new Typed(".typing",{
+    strings:["Web Developer","Frontend Developer","Backend Developer","Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
